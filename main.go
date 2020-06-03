007b6ed9fd8f8b6ece73d288efdab9a65586489b
rayne22 <changalesa8@gmail.com>
2020-06-03 13:34:03 +0200

    rtsa fix

--- main.go
+++ main.go
@@ -9,23 +9,25 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
-//type Params struct {
-//	InsuranceType uint `json:"insurance_type"`
-//	Status uint `json:"status"`
-//	RegistrationMark string `json:"registration_mark"`
-//	DateFrom time.Time `json:"date_from"`
-//	DateTo time.Time `json:"date_to"`
-//	InsurancePolicyNo string `json:"insurance_policy_no"`
-//	ChassisNumber string `json:"chassis_number"`
-//}
-//
-//var (
-//
-//	parA []Params
-//	jsonBuffer []byte
-//)
+type Params struct {
+	InsuranceType uint `json:"insuranceType"`
+	Status uint `json:"status"`
+	RegistrationMark string `json:"registrationMark"`
+	DateFrom time.Time`json:"dateFrom"`
+	DateTo time.Time `json:"dateTo"`
+	InsurancePolicyNo string `json:"insurancePolicyNo"`
+	ChassisNumber string `json:"chassisNumber"`
+}
+
+
+var (
+
+	parA []Params
+	jsonBuffer []byte
+)
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
@@ -35,38 +37,26 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 
 
 var rstaPost = http.HandlerFunc(func (response http.ResponseWriter, request *http.Request)  {
+
 	response.Header().Set("content-type", "application/json")
-	queryValues := request.URL.Query()
-	_, _ = fmt.Fprintf(response, "hello, %s!\n", queryValues.Get("registrationMark"))
-
-	insuranceType := queryValues.Get("insuranceType")
-	status := queryValues.Get("status")
-	registrationMark := queryValues.Get("registrationMark")
-	dateFrom := queryValues.Get("dateFrom")
-	dateTo := queryValues.Get("dateTo")
-	insurancePolicyNo := queryValues.Get("insurancePolicyNo")
-	chassisNumber := queryValues.Get("chassisNumber")
-
+	var messages []Params
 	var username  = "Douglas.Chilungu"
 	var passwd = "aplusgeneral@2019"
 	client := &http.Client{}
+	_ = json.NewDecoder(request.Body).Decode(&messages)
+	var ppl []interface{}
+	for _, p := range messages {
+		ppl = append(ppl, p)
+	}
 
-	var newA []string
+	_ = json.NewEncoder(response).Encode(ppl)
 
-	newA = append(newA, insuranceType)
-	newA = append(newA, status)
-	newA = append(newA, registrationMark)
-	newA = append(newA, dateFrom)
-	newA = append(newA, dateTo)
-	newA = append(newA, insurancePolicyNo)
-	newA = append(newA, chassisNumber)
 
-	log.Println("New A", newA)
+	jsonReq, err := json.Marshal(ppl)
 
-	nP, _ := json.Marshal( &newA)
 
 	url := "https://zampointzidb.eservices.gov.zm/ZIDB/ReceiveInsurancePolicies"
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(nP))
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonReq))
 
 	log.Println(request.Body)
 
@@ -75,15 +65,14 @@ var rstaPost = http.HandlerFunc(func (response http.ResponseWriter, request *htt
 
 	res, err := client.Do(req)
 	if err != nil{
-				log.Fatal(err)
-			}
+		log.Fatal(err)
+	}
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
 	fmt.Println(string(body))
+
 })
-
-
 
 
 
@@ -91,12 +80,12 @@ var rstaPost = http.HandlerFunc(func (response http.ResponseWriter, request *htt
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/rtsa", rstaPost).Methods("GET")
+	router.HandleFunc("/rtsa", rstaPost).Methods("Post")
 
 	port := os.Getenv("PORT")
 
 	log.Println(port)
-	log.Fatal(http.ListenAndServe(":" + port, router))
+	log.Fatal(http.ListenAndServe(":8060" , router))
 }
 
 
