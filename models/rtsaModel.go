f9bc97d2f90f0d01a6be6d8a92de958d88b3e1ef
Daniel Sitali <danielmsitali@outlook.com>
2020-10-05 09:13:02 +0200

    fdgdfg

--- models/rtsaModel.go
+++ models/rtsaModel.go
@@ -7,12 +7,16 @@ import (
 type Params struct {
 	InsuranceType uint `json:"insuranceType"`
 	Status uint `json:"status"`
-	RegistrationMark string `json:"registrationMark"`
 	DateFrom time.Time`json:"dateFrom"`
 	DateTo time.Time `json:"dateTo"`
 	InsurancePolicyNo string `json:"insurancePolicyNo"`
 	ChassisNumber string `json:"chassisNumber"`
+	Quarter uint `json:"quarter"`
+	InsuranceCompany string `json:"insuranceCompany"`
+	InsuranceCompanyId uint `json:"insuranceCompanyId"`
+	RegistrationMark string `json:"registrationMark"`
 }
+
 
 
 var (
